428f7628ff8391fa95856014fad9589d831d456d
davidrodriguez88 <davidxrodriguez@me.com>
2025-03-28 14:50:57 -0400

    Added and completed the fields

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "David";
+            int age = 36;
+            double avg = 98.7;
+            decimal bankAccount = 1000000.00m;
+            char initial = 'D';
+            bool isMarried = true;
+            
+            Console.WriteLine($"Name: {myName}\nAge: {age}\nAverage: {avg}\nMoney: {bankAccount}\nInitial: {initial}\nMarried: {isMarried}");
+            
         }
     }
 }
